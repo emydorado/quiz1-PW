01f290237d5650276e1cfe0e01522b6c17ce48b0
Emy Dorado <mac@MacBook-Air-de-Emy.local>
2025-02-17 18:08:34 -0500

    quiz corregido

--- quiz.js
+++ quiz.js
@@ -1,44 +1,52 @@
-const words = [
-    'apple',
-    'mango',
-    'board',
-    'shirt',
-    'coffee',
-    'computer',
-]
+const words = ['apple', 'mango', 'board', 'shirt', 'coffee', 'computer'];
 
-const word = document.getElementById('word')
-const input = document.getElementById('inputLetter')
-const randomButton = document.getElementById('randomButton')
-const verifyButton = document.getElementById('verify')
-const mistakes = document.getElementById('mistakes')
-const tries = document.getElementById('tries')
+const word = document.getElementById('word');
+const input = document.getElementById('inputLetter');
+const randomButton = document.getElementById('randomButton');
+const verifyButton = document.getElementById('verify');
+const mistakes = document.getElementById('mistakes');
+const tries = document.getElementById('tries');
+const result = document.getElementById('result');
 
-let randomWord
-let numberTries = 0
-let splittedWords
-let blankWords 
+let randomWord;
+let numberTries = 0;
+let splittedWords;
+let blankWords;
 
 randomButton.addEventListener('click', function () {
-    const numberRandom = Math.floor(Math.random() * words.length)
-    randomWord = words[numberRandom];
-    splittedWords = randomWord.split("")
-    console.log(randomWord);
+	const numberRandom = Math.floor(Math.random() * words.length);
+	randomWord = words[numberRandom];
+	splittedWords = randomWord.split('');
+	console.log(randomWord);
 
-    blankWords = splittedWords.fill('_')
-    word.textContent=blankWords
+	blankWords = Array(splittedWords.length).fill('_'); // se hace el metodo array para poder crear una copia en lugar de modificar elarray original y así poder hacer la comparación al final
+	word.innerText = blankWords;
+});
 
-})
+displayWord = () => {
+	word.textContent = blankWords;
+};
 
 verifyButton.addEventListener('click', function () {
-    const matchLetter = randomWord.includes(input.value);
+	const matchLetter = randomWord.includes(input.value);
+	let letter = input.value;
 
-    if (matchLetter) {
-        console.log(input.value);
-    }else{
-        numberTries++
-        tries.textContent=numberTries
-        mistakes.textContent+=input.value
-    }
-})
+	if (matchLetter) {
+		splittedWords.forEach((char, index) => {
+			if (char === letter) {
+				blankWords[index] = letter;
+			}
+		});
+	} else {
+		numberTries++;
+		tries.textContent = numberTries;
+		mistakes.textContent += input.value;
+	}
+	displayWord();
 
+	if (!blankWords.includes('_')) {
+		result.textContent = '¡Ganaste!';
+	} else if (numberTries >= 6) {
+		result.textContent = '¡Perdiste!';
+	}
+});
